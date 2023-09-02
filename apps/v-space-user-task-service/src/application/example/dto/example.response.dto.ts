e0614dde9bbdeac60ab6518efd2a4b067625db24
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-02 18:54:22 +0700

    commit

---
+++ apps/v-space-user-task-service/src/application/example/dto/example.response.dto.ts
@@ -0,0 +1,34 @@
+import {
+    NextFunction,
+    Request,
+    Response,
+    ValidateError,
+} from '@gln-libs/node-infrastructure';
+
+export const errorHandlerMiddleware = async (
+    error: Error,
+    _: Request,
+    res: Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    __: NextFunction
+): Promise<void> => {
+    global.logger.error(error);
+
+    if (!res.headersSent) {
+        let message = error?.message;
+        let statusCode = 400;
+        const detail: Record<string, unknown> = {};
+
+        if (error instanceof ValidateError) {
+            statusCode = 422;
+            if (process.env.NODE_ENV === 'production') {
+                message = 'Invalid format data';
+            } else {
+                message = error.firstErrorMessage;
+                Object.assign(detail, error.toJson());
+            }
+        }
+
+        res.resError(message, detail, { statusCode });
+    }
+};
