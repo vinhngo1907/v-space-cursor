30de57a0f2b1dfe2187f5e5c50ec6b3182daaec9
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-02 17:18:38 +0700

    commit

--- apps/v-space-user-task-service/src/datasource/postgresql/repository/example.ts
+++ apps/v-space-user-task-service/src/datasource/postgresql/repository/example.ts
@@ -1,1 +1,11 @@
-import {createBaseRepository} from "@v-libs/node-infrastructure";
+import { BaseEntity } from "@v-libs/node-infrastructure";
+import { Column, Entity } from "typeorm";
+
+@Entity('example')
+export class ExampleEntity extends BaseEntity {
+    @Column({
+        type: "varchar",
+        length: 50
+    })
+    name: string;
+}
