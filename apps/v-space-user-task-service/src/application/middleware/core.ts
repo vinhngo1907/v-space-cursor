8e7a6e2795d958e1d76d9ad3456edeb87a0954e6
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-02 19:00:21 +0700

    user task middleware

--- apps/v-space-user-task-service/src/application/middleware/core.ts
+++ apps/v-space-user-task-service/src/application/middleware/core.ts
@@ -1,10 +1,7 @@
-const { ValidateSignature } = require("../../utils");
+import cors from 'cors';
+import { Handler } from 'express';
 
-module.exports = async (req, res, next) => {
-    const isAuthorized = await ValidateSignature(req);
-
-    if (isAuthorized) {
-        return next();
-    }
-    return res.status(403).json({ message: "Not Authorized" });
-}
+export const corsMiddleware = (options?: cors.CorsOptions): Handler =>
+    cors({
+        ...options,
+    });
