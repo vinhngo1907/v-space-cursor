25fee4728685427523c39cba338e7a017cfdca4b
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-17 16:09:28 +0700

    user auth controller

--- apps/v-space-auth-service/src/application/userAuth/userAuth.controller.ts
+++ apps/v-space-auth-service/src/application/userAuth/userAuth.controller.ts
@@ -10,6 +10,12 @@ import {
 import * as bcrypt from 'bcrypt';
 import { UserAuth } from './interface/userAuth';
 import { UserAuthService } from './userAuth.service';
+import { SignUpRequestBodySchema } from './dto/userAuth.request.dto';
+
+function generateHash(str: string, saltRounds: 10) {
+    const hashStr = bcrypt.hashSync(str, saltRounds);
+    return hashStr;
+}
 
 @Controller('/auth')
 export class UserAuthController extends HttpController {
@@ -17,5 +23,20 @@ export class UserAuthController extends HttpController {
     constructor(router: Router) {
         super(router);
         this.userAuthService = new UserAuthService();
+    }
+    @Route('post')
+    @HttpBodyValidate(SignUpRequestBodySchema)
+    async create(req: Request, response: Response): Promise<void> {
+        const queriedUserAuth = await this.userAuthService.getByEmail(req.body.email);
+        if (queriedUserAuth) {
+            response.resError('Email already exists');
+        }
+
+        const password = generateHash(req.body.password, 10);
+        const payload: UserAuth = { ...req.body, password };
+
+        const userAuth = await this.userAuthService.createUserAuth(payload);
+
+        userAuth ? response.resSuccess(userAuth) : response.resError('Signup failure');
     }
 }
