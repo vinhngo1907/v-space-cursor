33f13dabc91a39f8638caf6e54571315b4203d8b
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-17 17:16:59 +0700

    import needed module to example controller

--- apps/v-space-user-task-service/src/application/example/example.controller.ts
+++ apps/v-space-user-task-service/src/application/example/example.controller.ts
@@ -1,5 +1,6 @@
-import { Controller } from "@v-libs/node-infrastructure";
+import { Controller, HttpController } from "@v-libs/node-infrastructure";
 import { CreateExampleRequestBodySchema } from './dto/example.request.dto';
+import { ExampleService } from "./example.service";
 @Controller('/example')
 export class ExampleController extends HttpController {
     exampleService: ExampleService
