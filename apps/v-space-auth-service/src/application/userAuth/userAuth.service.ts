fc71e18a88f71f0e8d1ff228aa78019aadf8ab2b
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-17 16:02:06 +0700

    create user auth

--- apps/v-space-auth-service/src/application/userAuth/userAuth.service.ts
+++ apps/v-space-auth-service/src/application/userAuth/userAuth.service.ts
@@ -4,7 +4,13 @@ import { UserAuth, UserAuthRepsone } from './interface/userAuth';
 // import { UserAuthRepository } from '@datasource/postgresql';
 
 export class UserAuthService {
-    async createUserAuth(payload: UserAuth):Promise<void>{
+    async createUserAuth(payload: UserAuth):Promise<UserAuthRepsone>{
+        const userAuth = UserAuthRepository.create({...payload, status: 1});
 
+        const userEntity = await UserAuthRepository.save(userAuth);
+
+        delete userEntity.password;
+
+        return UserAuthFromEntityDTO.toClass(userEntity);
     }
 }
