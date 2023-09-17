e9c05db6d1cb19e0d18cb235e2127ed96a529953
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-17 16:33:09 +0700

    user task request dto

--- apps/v-space-user-task-service/src/application/example/example.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Controller } from "@v-libs/node-infrastructure";
-import { CreateExampleRequestBodySchema } from './dto/example.request.dto';
-@Controller('/example')
-export class ExampleController extends HttpController {
-    exampleService: ExampleService
-}
