d9c8f26db5c6ac03b3928f5a568f2b7a7e14d599
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-17 17:40:36 +0700

    user task update

--- apps/v-space-user-task-service/src/application/userTask/userTask.service.ts
+++ apps/v-space-user-task-service/src/application/userTask/userTask.service.ts
@@ -7,6 +7,7 @@ import {
 import { UserTaskFromEntityDTO } from "./dto/userTask.response.dto";
 
 import {
+    UpdateUserTaskPayload,
     UserTask,
     UserTaskServiceRes
 } from "./interface/userTask";
@@ -26,13 +27,47 @@ export class UserTaskService {
         return true;
     }
 
-    async createUserTask(payload: UserTask):Promise<UserTaskServiceRes>{
-        const {startTime, endTime}= payload;
-        if(!this.validateUserTask({startTime, endTime})) return;
-        
+    async createUserTask(payload: UserTask): Promise<UserTaskServiceRes> {
+        const { startTime, endTime } = payload;
+        if (!this.validateUserTask({ startTime, endTime })) return;
+
         const userTask = UserTaskRepository.create(payload);
         const userTaskEntity = await UserTaskRepository.save(userTask);
 
         return UserTaskFromEntityDTO.toClass(userTaskEntity);
+    }
+
+    async updateUserTask(payload: UpdateUserTaskPayload, id: string): Promise<UserTaskServiceRes> {
+        if (Object.keys(payload).length === 0) return;
+
+        const { startTime, endTime } = payload;
+
+        if (startTime && !this.validateUserTask({ startTime, endTime })) return;
+
+        const queryRunner = primaryDataSource.createQueryRunner();
+        await queryRunner.connect();
+
+        await queryRunner.startTransaction();
+
+        const userTaskResult = await queryRunner.manager.update(UserTaskEntity, id, payload);
+
+        if (userTaskResult.affected === 0) return;
+
+        const updatedEntity = await queryRunner.manager.findOneBy(UserTaskEntity, { id });
+        if (
+            !this.validateUserTask({
+                startTime: updatedEntity.startTime,
+                endTime: updatedEntity.endTime,
+            })
+        ) {
+            await queryRunner.rollbackTransaction();
+            await queryRunner.release();
+            return;
+
+        }
+        await queryRunner.commitTransaction();
+        await queryRunner.release();
+
+        return UserTaskFromEntityDTO.toClass(updatedEntity);
     }
 }
