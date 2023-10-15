55ac382893b6d4aa5e6ea48514ee52f1db1e78ad
DESKTOP-VCJF8F8\vinhn <vinhtrungngo1907@gmail.com>
2023-10-15 21:17:57 +0700

    (▰˘◡˘▰)

--- apps/v-space-auth-service/src/application/configs/index.ts
+++ apps/v-space-auth-service/src/application/configs/index.ts
@@ -1,9 +1,24 @@
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
+
+import { join } from "path";
+import "dotenv/config";
 
 const env = process.env;
 const { NODE_ENV } = env;
+
+type JWT_CONFIG = {
+    secret: string;
+    signOptions: {
+        expiresIn: string;
+    }
+}
+
+type SeedingOptions = {
+    seeds: string[];
+    factories: string[];
+};
 
 export const AppConfig = {
     HttpServer: {
@@ -16,3 +31,51 @@ export const AppConfig = {
         },
     },
 } as const;
+
+
+class ConfigService {
+    constructor(private env: { [k: string]: string | undefined }) { }
+    private getValue(key: string, throwOnMissing = true): string {
+        const value = this.env[key];
+        if (!value && throwOnMissing) {
+            throw new Error(`config error - missing env.${key}`);
+        }
+        return value;
+    }
+
+    public ensureValues(keys: string[]) {
+        keys.forEach(key => this.getValue(key, true));
+        return this;
+    }
+
+    public getPort() {
+        return this.getValue('HTTP_SERVER_PORT', true);
+    }
+
+    public isProduction() {
+        const mode = this.getValue('MODE', true);
+    }
+
+    // public getTypeOrmConfig(): TypeOrmMo { }
+
+    public getJwtConfig(): JWT_CONFIG {
+        return {
+            secret: this.getValue('JWT_SECRET'),
+            signOptions: {
+                expiresIn: `${this.getValue('JWT_EXPIRATION_TIME')}s` || '60s',
+            }
+        }
+    }
+}
+
+const configService = new ConfigService(process.env).ensureValues([
+    'POSTGRES_HOST',
+    'POSTGRES_PORT',
+    'POSTGRES_USER',
+    'POSTGRES_PASSWORD',
+    'POSTGRES_DB',
+    'JWT_SECRET',
+    'JWT_EXPIRATION_TIME',
+]);
+
+export { configService };
